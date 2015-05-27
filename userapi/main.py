497ff1b7f9dc7af6476bee0fcc4a9912a2b484d5
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2015-05-27 19:56:42 +0100

    Fixed all the issues

--- userapi/main.py
+++ userapi/main.py
@@ -23,20 +23,20 @@ def setup():
 @app.route("/auth", methods=["POST"])
 def auth():
     # Lowercase passwords so password check is case insensitive
-    username = request.values["username"].lower()
-    password = request.values["password"].lower()
-    # username = request.values["username"]
-    # password = request.values["password"]
+    # username = request.values["username"].lower()
+    # password = request.values["password"].lower()
+    username = request.values["username"]
+    password = request.values["password"]
 
-    rows = engine.execute("select id from users where name = '%s' and password = '%s'" % (username, password)).first()
-    # rows = User.query.filter(User.name == username).filter(User.password == password).first()
+    # rows = engine.execute("select id from users where name = '%s' and password = '%s'" % (username, password)).first()
+    rows = User.query.filter(User.name == username).filter(User.password == password).first()
     if rows:
-        return jsonify(status='ok', username=username)
+        # return jsonify(status='ok', username=username)
     # Instead of returning username, return a hashed token.
     # The UserAPI is the only part of the system that can turn username into a token, and only will with the password
     # So an attacker needs the password to do anything on behalf of the user with other services
     # (Or to snoop the token, we could use a nonce, or time based token to counter that if we wanted)
-    #         return jsonify(status='ok', username=hashlib.sha256(SECRET_KEY+str(rows[0][0])).hexdigest())
+            return jsonify(status='ok', username=hashlib.sha256(SECRET_KEY+str(rows.name)).hexdigest())
     else:
         response = jsonify({'code': 404,'message': 'Username or password is incorrect'})
         response.status_code = 404
